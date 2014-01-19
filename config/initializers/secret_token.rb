7af21b5b90a49882b831241d8ea2d9399cdd409a
asher <asher@asher-OptiPlex-780.(none)>
2014-01-19 13:12:07 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicropostApp::Application.config.secret_token = '07ade8c0a148c6bcf56387934ea5980c5f74c3214f061ea1a525811137b20f9dd8c6d6de0f0870a95c1581737b4880eca6743c1065e88eaff24adcd42d5f9387'
