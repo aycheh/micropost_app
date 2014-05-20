5a5f9332c89107bc145eb3cbae9a9f4d73eb8713
asher <asher@asher-OptiPlex-780.(none)>
2014-05-20 18:27:56 +0300

    fix db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140518145606) do
+ActiveRecord::Schema.define(:version => 20140520151141) do
 
   create_table "users", :force => true do |t|
-    t.string   "mane"
+    t.string   "name"
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
